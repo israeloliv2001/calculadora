1fc92800ae3c129009066b05582361eb46495035
Israel Oliveira <israel.baterista@icloud.com>
2026-02-17 21:47:12 -0300

    primeira versão

---
+++ script.js
@@ -0,0 +1,25 @@
+function inserir(num) {
+    document.querySelector('.tela').innerHTML += num;
+};
+
+function limpar() {
+    document.querySelector('.tela').innerHTML = '';
+};
+
+function back() {
+    let tela = document.querySelector('.tela').innerHTML;
+    document.querySelector('.tela').innerHTML = tela.substring(0, tela.length -1
+
+    );
+    
+};
+
+function calcular() {
+    let tela = document.querySelector('.tela').innerHTML;
+
+    try {
+        document.querySelector('.tela').innerHTML = eval(tela);
+    } catch {
+        document.querySelector('.tela').innerHTML = 'Erro';
+    };
+};
